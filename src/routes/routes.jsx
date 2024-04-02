43e76691593fb588baa1c01e9489b582d11927f4
KNereS <neres.mp3@gmail.com>
2024-04-02 17:01:55 -0300

    editando rota para produção

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -9,7 +9,7 @@ const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
